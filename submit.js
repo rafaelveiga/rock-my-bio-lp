82188c87c147b31126ebf7ade7b2472e828e799e
Rafael Veiga <dsg.rafael@gmail.com>
2023-05-28 15:19:02 -0300

    init email collection script

---
+++ submit.js
@@ -0,0 +1,6 @@
+const button = document.getElementById("submit");
+button.addEventListener("click", () => {
+  const input = document.getElementById("input");
+  const value = input.value;
+  console.log(value);
+});
